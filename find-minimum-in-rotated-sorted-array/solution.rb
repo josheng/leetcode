d5fc628ec4e27b5e0a4e0e6f53a729868d791363
Joshua Eng <engch28@gmail.com>
2022-02-17 23:43:01 +0800

    added details

--- find-minimum-in-rotated-sorted-array/solution.rb
+++ find-minimum-in-rotated-sorted-array/solution.rb
@@ -8,11 +8,18 @@ def find_min(nums)
   low = 0
   high = nums.length - 1
   while true
+    # find the middle index of the array
     mid = ((high - low) / 2) + low
+    # set low to mid if the lower index element is lesser then middle index element
+    # because the array is in ascending order, we can assume anything inbetween is in sequence
     if nums[low] < nums[mid]
       low = mid
+    # if low index element is greater then middle index, we set high to mid
+    # this will allow us to narrow down to the point of rotated array
     elsif nums[low] > nums[mid]
       high = mid
+    # if low is equals to mid then we know that we have already reach the point when there is only 2 index left
+    # so if low index element is greater then high index element, we have found the point of rotation
     elsif low == mid && nums[low] > nums[high]
       # below 2 lines will return number of rotation instead
       # new_array = nums[high..-1]
