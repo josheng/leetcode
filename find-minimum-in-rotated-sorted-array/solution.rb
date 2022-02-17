787c77d9923ce1a819d989835604539d490001e7
Joshua Eng <engch28@gmail.com>
2022-02-17 23:32:17 +0800

    completed find minimum in rotated sorted array

--- find-minimum-in-rotated-sorted-array/solution.rb
+++ find-minimum-in-rotated-sorted-array/solution.rb
@@ -1,15 +1,29 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_min(nums)
-    # nums.sort.first
-    return nums.first if nums.first < nums.last
-    low = 0
-    high = nums.length - 1
-    while true
-      mid = ((high - low) / 2) + low
+  # nums.sort.first
+  return nums.first if nums.first < nums.last
+  return nums.first if nums.length == 1
 
+  low = 0
+  high = nums.length - 1
+  while true
+    mid = ((high - low) / 2) + low
+    if nums[low] < nums[mid]
+      low = mid
+    elsif nums[low] > nums[mid]
+      high = mid
+    elsif low == mid && nums[low] > nums[high]
+      # below 2 lines will return number of rotation instead
+      # new_array = nums[high..-1]
+      # return new_array.length
+      return nums[high]
     end
+  end
 end
 
-nums = [3,4,5,1,2]
+# nums = [3,4,5,6,7,8,1,2]
+# nums = [2,1]
+# nums = [5,1,2,3,4]
+
 p find_min(nums)
