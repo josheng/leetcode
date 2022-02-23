a530105dbd7cd5883111ecc82387731d11545441
Joshua Eng <engch28@gmail.com>
2022-02-24 02:44:13 +0800

    use each to increase speed

--- best-time-buy-sell-stock/solution.rb
+++ best-time-buy-sell-stock/solution.rb
@@ -4,9 +4,13 @@ def max_profit(prices)
   min_value = prices.first
   profit = 0
 
-  1.upto(prices.length - 1) do |index|
-    profit = prices[index] - min_value if (prices[index] - min_value) > profit
-    min_value = prices[index] if prices[index] < min_value
+  # 1.upto(prices.length - 1) do |index|
+  #   profit = prices[index] - min_value if (prices[index] - min_value) > profit
+  #   min_value = prices[index] if prices[index] < min_value
+  # end
+  prices.each do |value|
+    profit = value - min_value if (value - min_value) > profit
+    min_value = value if value < min_value
   end
   profit
 end
